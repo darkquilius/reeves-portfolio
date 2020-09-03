5f2fea81b0d8d9a14ad4734935bd2652934de195
Andrew Reeves <andrew.reeves.coding.bootcamp@gmail.com>
2020-09-03 19:42:13 -0400

    Updates

---
+++ precache-manifest.2fc6afe0d0c955de252fc0edcd6d4cee.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd457cd04949dee2f5d2eaf96bcc15ef",
+    "url": "/reeves-portfolio/index.html"
+  },
+  {
+    "revision": "39b3f4201bdb60170c48",
+    "url": "/reeves-portfolio/static/css/main.bee27362.chunk.css"
+  },
+  {
+    "revision": "e8b31ec30774717920ad",
+    "url": "/reeves-portfolio/static/js/2.8e465edf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reeves-portfolio/static/js/2.8e465edf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39b3f4201bdb60170c48",
+    "url": "/reeves-portfolio/static/js/main.8a0ee9a6.chunk.js"
+  },
+  {
+    "revision": "32047d0afed11503c116",
+    "url": "/reeves-portfolio/static/js/runtime-main.cfac7a12.js"
+  },
+  {
+    "revision": "c0264e2df797fa67259992e73c3b122b",
+    "url": "/reeves-portfolio/static/media/Client-Clock-Website.c0264e2d.png"
+  },
+  {
+    "revision": "35dcd13ef216d560d66ffb427b48d342",
+    "url": "/reeves-portfolio/static/media/Daily-Planner-Website.35dcd13e.png"
+  },
+  {
+    "revision": "c787748c46e0e75e1cf11182609c5634",
+    "url": "/reeves-portfolio/static/media/TeamSprinter.c787748c.png"
+  },
+  {
+    "revision": "ff97f6fcb706f8c8ece0df00e913e761",
+    "url": "/reeves-portfolio/static/media/Weather-Website.ff97f6fc.png"
+  },
+  {
+    "revision": "6738cba174de7b802cef7c53ab4066f4",
+    "url": "/reeves-portfolio/static/media/myPhoto.6738cba1.png"
+  }
+]);
